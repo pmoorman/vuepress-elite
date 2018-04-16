9f2555c6f0ddd26eb11c5ce339c7441013ce7d3a
pmoorman <pietermoorman@hotmail.com>
2018-04-16 16:16:26 +0700

    try add sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,11 @@
-// config.js
-
 module.exports = {
   title: 'Elite',
-  description: 'A course on startup growth'
+  description: 'A course on startup growth',
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/page-a',
+      ['/page-b', 'Explicit link text']
+    ]
+  }
 }
