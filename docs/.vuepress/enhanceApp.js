f2e2127aa6b00e157073d97f230e0c05e07ab05f
pmoorman <pietermoorman@hotmail.com>
2019-03-20 11:51:34 -0400

    TEST: exactly match his work

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,12 +1,48 @@
 import VueLocalStorage from 'vue-localstorage';
 
 export default ({
-  Vue, // the version of Vue being used in the VuePress app
-  options, // the options for the root Vue instance
-  router, // the router instance for the app
-  siteData // site metadata
-}) => {
+                  Vue, // the version of Vue being used in the VuePress app
+                  options, // the options for the root Vue instance
+                  router, // the router instance for the app
+                  siteData // site metadata
+                }) => {
   Vue.use(VueLocalStorage);
+
+
+  /*
+  router.beforeEach((to, from, next) => {
+    console.log('here route goes');
+    console.log(to);
+
+    if (to.path === '/') {
+      if (typeof window !== "undefined") {
+        window.setTimeout(function () {
+          next();
+        }, 1000)
+      } else {
+        next();
+      }
+    }
+    else {
+      next();
+    }
+  });
+
+
+  router.afterEach((to, from) => {
+    let auth = Vue.localStorage && Vue.localStorage.get('p');
+
+    // If not authorized redirect to login page
+    if (!auth) {
+      // On SSR window is not defined
+      if (typeof window !== "undefined") {
+        window.location.href = '/login.html';
+      }
+    }
+  });
+
+  */
+
 
   router.beforeEach((to, from, next) => {
     let auth = Vue.localStorage && Vue.localStorage.get('p');
