41c6b820e28d109d0d8331e47157c974c0e9a4c0
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2024-01-31 19:49:02 +0700

    Remove login

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,10 @@
-import VueLocalStorage from 'vue-localstorage';
+import VueLocalStorage from "vue-localstorage";
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
-  siteData // site metadata
+  siteData, // site metadata
 }) => {
   Vue.use(VueLocalStorage);
 
@@ -42,35 +42,34 @@ export default ({
 
   */
 
+  // router.beforeEach((to, from, next) => {
+  //   let auth = Vue.localStorage && Vue.localStorage.get('p');
 
-  router.beforeEach((to, from, next) => {
-    let auth = Vue.localStorage && Vue.localStorage.get('p');
+  //   // If not authorized redirect to login page
+  //   if (!auth) {
+  //     // On SSR window is not defined
+  //     if (typeof window !== "undefined") {
+  //       window.location.href = '/login.html';
+  //     }
+  //   } else {
+  //     document.body.style.display = 'block';
+  //     document.title = 'Before we get started';
+  //   }
 
-    // If not authorized redirect to login page
-    if (!auth) {
-      // On SSR window is not defined
-      if (typeof window !== "undefined") {
-        window.location.href = '/login.html';
-      }
-    } else {
-      document.body.style.display = 'block';
-      document.title = 'Before we get started';
-    }
+  //   if (auth && to.path == '/') {
+  //     window.setTimeout(function () {
+  //       document.title = 'Before we get started';
+  //     }, 1200);
+  //   }
 
-    if (auth && to.path == '/') {
-      window.setTimeout(function () {
-        document.title = 'Before we get started';
-      }, 1200);
-    }
+  //   if (to.path === '/' && typeof window !== "undefined") {
+  //     window.setTimeout(function () {
+  //       console.log('next is called 1second later');
+  //       next();
+  //     }, 1000);
+  //     return;
+  //   }
 
-    if (to.path === '/' && typeof window !== "undefined") {
-      window.setTimeout(function () {
-        console.log('next is called 1second later');
-        next();
-      }, 1000);
-      return;
-    }
-
-    next();
-  });
-}
+  //   next();
+  // });
+};
